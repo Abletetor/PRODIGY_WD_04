0e3a3d8b00d0e318ebfd3731dacd060d16bd229f
Abletetor <abletetor@gmail.com>
2024-11-26 15:54:23 +0000

    Add new list of project component

---
+++ src/Components/Project-List/ProjectData.jsx
@@ -0,0 +1,59 @@
+import herbalImg from '../../assets/hebal.png';
+import quoteImg from '../../assets/quote.png';
+import authImg from '../../assets/auth.png';
+import cryptozImg from '../../assets/cryptoz.png';
+import surveytrackerImg from '../../assets/surveytracker.png';
+import tasculImg from '../../assets/tascul.png';
+
+const projectData = [
+   {
+      title: "HerbalMedics",
+      description: "A responsive landing page showcasing herbal products with smooth navigation, product details, and testimonials.",
+      techStack: "React, CSS, HTML, Git",
+      github: 'https://github.com/Abletetor/HerbalMedic',
+      demo: 'https://herbalmedics.netlify.app/',
+      image: herbalImg,
+   },
+   {
+      title: 'CryptoZ',
+      description: 'CryptoZ is an intuitive platform for tracking real-time currency prices, market trends, and news. It offers insights into the crypto market with a sleek interface.',
+      techStack: "HTML, CSS, JavaScript, Git",
+      github: 'https://github.com/Abletetor/cryptoZ-project',
+      demo: 'https://cryptoz-project.netlify.app/',
+      image: cryptozImg,
+   },
+   {
+      title: 'SurveyTracker Portal',
+      description: 'A portal to manage and track survey responses with robust data visualization and management tools.',
+      techStack: 'EJS, NodeJs, ExpressJs, CSS, Git',
+      github: 'https://github.com/Abletetor/survey-tracker-portal',
+      demo: 'https://survey-tracker-portal.vercel.app/auth/login',
+      image: surveytrackerImg,
+   },
+   {
+      title: "Quote Generator",
+      description: "A simple, interactive web app that provides users with random motivational quotes at the click of a button. Built with modern web technologies, it features a sleek design and smooth functionality to inspire and uplift.",
+      techStack: "HTML, JavaScript, CSS, Git",
+      github: 'https://github.com/Abletetor/quote-generator',
+      demo: 'https://simplex-quotes-generator.netlify.app/',
+      image: quoteImg,
+   },
+   {
+      title: 'Tascul Redesign',
+      description: "A dynamic platform showcasing Tascul's services, offering a seamless user experience with responsive design, engaging visuals, and smooth navigation. It highlights innovative solutions and supports user interaction through modern web technologies.",
+      techStack: 'ReactJS, CSS, NodeJs, ExpressJs, RestAPIs, Git',
+      github: 'https://github.com/Abletetor/Tascul_Internship_01',
+      demo: 'tascul-redesign.netlify.app/',
+      image: tasculImg,
+   },
+   {
+      title: 'Auth-System',
+      description: 'A secure user authentication platform enabling seamless signup, login, and token-based access control. It supports role-based permissions, password recovery, and robust validation to safeguard user data.',
+      techStack: "ReactJs, CSS, NodeJs, ExpressJs, RestAPIs, Git",
+      github: 'https://github.com/Abletetor/auth-frontend',
+      demo: 'https://myauth-system.netlify.app/',
+      image: authImg,
+   },
+];
+export default projectData;
+
--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,64 +1,9 @@
 /* eslint-disable react/prop-types */
 import './Projects.css';
 import { FaGithub, FaExternalLinkAlt } from 'react-icons/fa';
-import herbalImg from '../../assets/hebal.png';
-import stopwatchImg from '../../assets/stopwatch.png';
-import tictactoeImg from '../../assets/tic-tac-toe.png';
-import cryptozImg from '../../assets/cryptoz.png';
-import surveytrackerImg from '../../assets/surveytracker.png';
-import weatherAppImg from '../../assets/weatherApp.png';
+import projectData from '../Project-List/ProjectData';
 
 const Projects = ({ darkMode }) => {
-   const projectData = [
-      {
-         title: "HerbalMedics",
-         description: "A responsive landing page showcasing herbal products with smooth navigation, product details, and testimonials.",
-         techStack: "React, CSS, HTML",
-         github: 'https://github.com/Abletetor/HerbalMedic',
-         demo: 'https://herbalmedics.netlify.app/',
-         image: herbalImg,
-      },
-      {
-         title: 'CryptoZ',
-         description: 'CryptoZ is an intuitive platform for tracking real-time cryptocurrency prices, market trends, and news. It offers insights into the crypto market with a sleek interface and user-friendly tools to make informed decisions.',
-         techStack: "HTML, CSS, JavaScript",
-         github: 'https://github.com/Abletetor/cryptoZ-project',
-         demo: 'https://cryptoz-project.netlify.app/',
-         image: cryptozImg,
-      },
-      {
-         title: 'SurveyTracker Portal',
-         description: 'A portal to manage and track survey responses with robust data visualization and management tools.',
-         techStack: 'EJS, Node.js, Express, CSS',
-         github: 'https://github.com/Abletetor/survey-tracker-portal',
-         demo: 'https://survey-tracker-portal.vercel.app/auth/login',
-         image: surveytrackerImg,
-      },
-      {
-         title: "Stopwatch",
-         description: "A sleek stopwatch app with start, pause, reset, and lap tracking functionalities built with React.",
-         techStack: "React, JavaScript, CSS",
-         github: 'https://github.com/Abletetor/PRODIGY_WD_02',
-         demo: 'https://prodigy-wd-02-stopwatch-sable.vercel.app/',
-         image: stopwatchImg,
-      },
-      {
-         title: 'WeatherApp',
-         description: 'A responsive Weather App that fetches real-time weather data using the OpenWeather API, featuring dark mode, real-time updates for humidity, wind speed, and more. The app has a sleek, modern UI optimized for all devices.',
-         techStack: 'ReactJS, CSS, Node.js, Express',
-         github: 'https://github.com/Abletetor/PRODIGY_WD_05',
-         demo: 'https://prodigy-wd-05-coral.vercel.app/',
-         image: weatherAppImg,
-      },
-      {
-         title: 'Tic-Tac-Toe',
-         description: 'A sleek, interactive Tic-Tac-Toe game built with ReactJS. Play against a human or AI opponent with dark mode support and responsive design.',
-         techStack: "ReactJS, CSS, JavaScript",
-         github: 'https://github.com/Abletetor/PRODIGY_WD_03',
-         demo: 'https://prodigy-wd-03-tic-tac-toe-rose.vercel.app/',
-         image: tictactoeImg,
-      },
-   ];
 
    return (
       <section className={ `projects ${darkMode ? 'dark' : ''}` } id="projects">
